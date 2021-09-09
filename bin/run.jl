56beed732933e141ac798e24c9d0c1a315936cfc
Caroline Hughes <caroline.hughes@nrel.gov>
2021-09-09 12:51:34 -0600

    updated bin/run.jl for current methods and features

--- bin/run.jl
+++ bin/run.jl
@@ -1,32 +1,48 @@
 using Waterfall
+
+# include("")
+include(joinpath(WATERFALL_DIR,"src","includes.jl"))
 include(joinpath(WATERFALL_DIR,"bin","io.jl"))
 
-# Define highlights to mark.
-prob = [0.75,0.25]
-highlights=["mean"; [("quantile",p) for p in prob]]
+# List of permutations.
+perms = vcat([[
+    ii,
+    swapat!(copy(ii), 1, 2),
+] for ii in [collect(1:4)]]...)
 
-for samples in [1,5,10,50]
-    pdata = Plot(df; numsample=samples, ylabel=:Units, kwargs...)
-    
-    # Iterate over plot type.
-    for T in [Horizontal,Parallel,Vertical,Violin]
-        p = Plot{T}(pdata)
 
-        # Show different combinations of labeling.
-        for hh in [[], [1], 1:length(highlights)]
-        # for hh in [[]]
-            (samples==1 && !isempty(hh)) && continue
+# !!!! Add support for defining a legend to violin plots.
+for T in [Horizontal, Vertical, Parallel]
+    for nsample in [1,10,50]
+        for colorcycle in [true,false]
+            for perm in perms
 
-            # f = filename(p, highlights[hh]; distribution=distribution)
-            Printf.@printf("\nPlotting and saving figure to %s", f)
+                (length(perm)>length(COLORCYCLE) && colorcycle) && continue
+                
+                # Define keyword arguments specific to this iteration.
+                # By default, permute and correlate kwargs = true.
+                global locals = (
+                    # What modifications should be made to the input data?
+                    nsample = nsample,
+                    permutation = perm,
+                    # How should the plot be formatted?
+                    colorcycle = colorcycle,
+                    ylabel = "Efficiency (%)",
+                )
+                
+                global cascade = define_from(Cascade{Data}, df; locals..., kwargs...)
+                global plot = define_from(Plot{T}, copy(cascade); locals..., kwargs...)
 
-            Luxor.@png begin
-                Luxor.fontsize(14)
-                Luxor.setmatrix([1 0 0 1 LEFT_BORDER TOP_BORDER])
+                Luxor.@png begin
+                    Luxor.fontface("Gill Sans")
+                    Luxor.fontsize(FONTSIZE)
+                    Luxor.setline(1.0)
+                    Luxor.setmatrix([1 0 0 1 LEFT_BORDER TOP_BORDER])
 
-                draw(p; distribution=distribution, samples=samples)
-
-            end WIDTH+LEFT_BORDER+RIGHT_BORDER HEIGHT+TOP_BORDER+BOTTOM_BORDER "newplot.png"
+                    draw(plot)
+                    
+                end WIDTH+LEFT_BORDER+RIGHT_BORDER HEIGHT+TOP_BORDER+BOTTOM_BORDER+padding(plot.axes[1]) plot.path
+            end
         end
     end
 end
--- dev/restructure.jl
+++ dev/restructure.jl
@@ -4,14 +4,13 @@ using Waterfall
 # include("")
 include(joinpath(WATERFALL_DIR,"src","includes.jl"))
 include(joinpath(WATERFALL_DIR,"bin","io.jl"))
-# include(joinpath(WATERFALL_DIR,"dev","legend.jl"))
 
 
-# function define_permute(df, nperm::T; kwargs...) where T<:Integer
-#     N = size(df,1)-2
-#     lst = [[collect(1:N)]; random_permutation(1:N, min(factorial(N),nperm))]
-#     return define_permute(df, lst; kwargs...)
-# end
+function define_permute(df, nperm::T; kwargs...) where T<:Integer
+    N = size(df,1)-2
+    lst = [[collect(1:N)]; random_permutation(1:N, min(factorial(N),nperm))]
+    return define_permute(df, lst; kwargs...)
+end
 
 function define_permute(df::DataFrames.DataFrame, lst::T; fun::Function=identity, kwargs...) where T<:AbstractArray
     cascades = Dict()
@@ -41,94 +40,4 @@ function define_permute(df::DataFrames.DataFrame, lst::T; fun::Function=identity
 
     set_value!.(data, vectorize(median))
     return cascade
-end
-
-nsample = 5
-
-perms = vcat([[
-    ii,
-    swapat!(copy(ii), 1, 2),
-] for ii in [collect(1:4)]]...)
-
-for T in [Horizontal]
-# for T in [Parallel]
-    # for nsample in [1,10,50]
-    for nsample in [5]
-        # for colorcycle in [true,false]
-        for colorcycle in [false]
-            for perm in [perms[end]]
-
-                (length(perm)>length(COLORCYCLE) && colorcycle) && continue
-                
-                global locals = (
-                    nsample = nsample,
-                    permutation = perm,
-                    permute = true,
-                    ncor = true,
-                    correlate = true,
-                    colorcycle = colorcycle,
-                    ylabel = "Efficiency (%)",
-                    usegradient = false,
-                )
-                
-                global cascade = define_from(Cascade{Data}, df; locals..., kwargs...)
-                global plot = define_from(Plot{T}, copy(cascade); locals..., kwargs...)
-
-                # --- STILL GOOD ---
-                # global legd = _push!(
-                #     _define_legend(plot.cascade; locals..., kwargs...),
-                #     cascade, T, Statistics.mean; locals..., kwargs...
-                # )
-
-                Luxor.@png begin
-                    Luxor.fontface("Gill Sans")
-                    Luxor.fontsize(FONTSIZE)
-                    Luxor.setline(1.0)
-                    Luxor.setmatrix([1 0 0 1 LEFT_BORDER TOP_BORDER])
-
-                    draw(plot)
-                    # draw(legd)
-                    # draw(h)
-                    # draw(a)
-                    # draw(ha)
-                    # [draw(h[ii].shape) for ii in 1:length(h)]
-                    # [draw(h[ii].label) for ii in 1:length(h)]
-
-                    # draw(lab)
-                    # draw(cas)
-
-                    # global s = _define_series(cas, Statistics.mean)
-                    # draw(s)
-
-                    # xshape = WIDTH-100;
-                    # yshape = 3*SEP
-                    # wid = 2*SEP
-                    
-                    # blend = [
-                    #     Box(
-                    #         (Luxor.Point(xshape-wid, yshape-SEP/2), Luxor.Point(xshape+wid, yshape+SEP/2)),
-                    #         _define_from(Coloring, HEX_GAIN),
-                    #         :fill,
-                    #     ),
-                    #     Box(
-                    #         (Luxor.Point(xshape-wid, yshape+FONTSIZE-SEP/2), Luxor.Point(xshape+wid, yshape+FONTSIZE+SEP/2)),
-                    #         _define_from(Coloring, HEX_LOSS),
-                    #         :fill,
-                    #     )
-                    # ]
-
-                    # leg = [
-                    #     _define_from(Label, "GAIN", Luxor.Point(xshape+wid+SEP, yshape); halign=:left, scale=0.8),
-                    #     _define_from(Label, "LOSS", Luxor.Point(xshape+wid+SEP, yshape+FONTSIZE); halign=:left, scale=0.8),
-                    # ]
-
-                    # draw(blend)
-                    # draw(leg)
-
-                    # nsample>1 && draw(cmean)
-                    
-                end WIDTH+LEFT_BORDER+RIGHT_BORDER HEIGHT+TOP_BORDER+BOTTOM_BORDER+padding(plot.axes[1]) plot.path
-            end
-        end
-    end
 end
